bd6a51cbc4a59d2414b70a9f98edd094e546749e
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2023-03-22 14:25:56 +0700

    Update source

--- booking-ticket/src/App.js
+++ booking-ticket/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import HomeTemplate from './templates/HomeTemplate/HomeTemplate';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import News from './pages/News/News';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<HomeTemplate />}>
+          <Route index path='' element={<Home />} ></Route>
+          <Route path='home' element={<Home />} ></Route>
+          <Route path='contact' element={<Contact />} ></Route>
+          <Route path='news' element={<News />} ></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='register' element={<Register />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
